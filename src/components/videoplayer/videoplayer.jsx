1ecb512541dcafc6431e9a246b2794a37bc1869e
rockn1989 <gold_100@bk.ru>
2021-02-19 21:43:22 +0300

    Больше подробностей (часть 1)

---
+++ src/components/videoplayer/videoplayer.jsx
@@ -0,0 +1,46 @@
+import React, {useEffect, useRef} from 'react';
+import propTypes from "prop-types";
+
+const VideoPlayer = (props) => {
+  const {posterImage, previewVideo, isActive} = props;
+  const video = useRef(null);
+
+  let timerId = null;
+
+  const videoStyle = {
+    width: `100%`,
+    height: `100%`,
+    objectFit: `cover`
+  };
+
+  useEffect(() => {
+    video.current.poster = posterImage;
+    video.current.src = previewVideo;
+
+    timerId = setTimeout(() => {
+      if (isActive) {
+        video.current.play();
+      } else {
+        video.current.pause();
+      }
+    }, 1000);
+
+    return () => {
+      video.current.currentTime = 0;
+      video.current.load();
+      clearInterval(timerId);
+    };
+  }, [isActive]);
+
+  return (
+    <video muted style={videoStyle} ref={video}></video>
+  );
+};
+
+VideoPlayer.propTypes = {
+  posterImage: propTypes.string.isRequired,
+  previewVideo: propTypes.string.isRequired,
+  isActive: propTypes.bool.isRequired
+};
+
+export default VideoPlayer;
