254bec2788f85d7739fcddf3e0691d790a84e91c
rockn1989 <gold_100@bk.ru>
2021-04-02 22:25:55 +0300

    правки на главной странице

--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -71,6 +71,9 @@ const Welcome = ({
                 <button
                   className="btn btn--play movie-card__button"
                   type="button"
+                  onClick={() => {
+                    history.push(`/player/${promo.id}`);
+                  }}
                 >
                   <svg viewBox="0 0 19 19" width="19" height="19">
                     <use xlinkHref="#play-s"></use>
